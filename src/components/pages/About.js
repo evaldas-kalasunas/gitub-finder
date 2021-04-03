d059ada54e482262dc96cff80cd2bfc59aa16699
oruxas <evaldaskal@gmail.com>
2021-04-03 18:35:31 +0300

    Github Finder With State version

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>About</h1>
+            <p>Aop to search git user's repos</p>
+            <i>Version: 1.0.0</i>
+        </Fragment>
+    )
+}
